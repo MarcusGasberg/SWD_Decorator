e5ecd32a5ed36e9277ff7833cdbfbd2b7c44e44f
Martin <martinx1995x@hotmail.com>
2019-04-09 11:01:22 +0200

    Attackable lavet

--- SWD_Decorator/SWD_Decorator/OffenciveEquipment.cs
+++ SWD_Decorator/SWD_Decorator/OffenciveEquipment.cs
@@ -6,9 +6,9 @@ namespace SWD_Decorator
     public class OffenciveEquipment : IEquipment, IAttack
     {
         protected readonly List<IStat> stats=new List<IStat>();
-        public void Attack(Attackable target)
+        public void Attack(IAttackable target)
         {
-            throw new System.NotImplementedException();
+           // target.Attack();
         }
 
         public T GetStat<T>() where T : IStat
