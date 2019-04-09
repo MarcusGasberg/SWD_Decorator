d81d28e7e936aca82676432f352348bb97bc7539
Martin <martinx1995x@hotmail.com>
2019-04-09 10:39:17 +0200

    offensive equipment start

--- SWD_Decorator/SWD_Decorator/OffenciveEquipment.cs
+++ SWD_Decorator/SWD_Decorator/OffenciveEquipment.cs
@@ -1,10 +1,20 @@
+using System.Collections.Generic;
+using System.Linq;
+
 namespace SWD_Decorator
 {
     public class OffenciveEquipment : IEquipment, IAttack
     {
+        protected readonly List<IStat> stats=new List<IStat>();
         public void Attack(Attackable target)
         {
             throw new System.NotImplementedException();
+        }
+
+        public T GetStat<T>() where T : IStat
+        {
+
+            return stats.OfType<T>().FirstOrDefault();
         }
     }
 }
